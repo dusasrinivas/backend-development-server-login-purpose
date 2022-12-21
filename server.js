7e8e3f112b27b7f541bd22ce3f321d6dbc412d5c
Srinivas <srinivasdsri1989@gmail.com>
2022-12-21 12:41:55 +0530

    added PORT variable to the server.js firle

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const app = express();
 app.use(express.json());
 
 let db = null;
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 const initializeDBAndServer = async () => {
   try {
     db = await open({ filename: dbPath, driver: sqlite3.Database });
-    app.listen(port, () => {
+    app.listen(PORT, () => {
       console.log("Server Running at http://localhost:3000/");
     });
   } catch (e) {
